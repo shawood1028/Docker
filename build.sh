b5508aa337146797c63a8ae62d395220fa39a408
shawood <1127295751@qq.com>
2022-04-01 16:53:17 +0800

    mysql image demo

--- build.sh
+++ build.sh
@@ -4,3 +4,8 @@
 docker build -f ./ubuntu/Dockerfile -t ubuntu:toshaw
 
 echo "---ubuntu is ok---"
+
+# build mysql images
+docker build -f ./mysql/Dockerfile -t mysql:toshaw
+
+echo "---mysql is ok---"
